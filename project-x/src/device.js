7cc32f306ef2318da333d1973eafb8acba63e17f
DmytroRoik <dmytroroik@gmail.com>
2018-03-27 01:16:17 +0300

    add wifi-enable/disable method

--- project-x/src/device.js
+++ project-x/src/device.js
@@ -1,7 +1,41 @@
 class Device {
+
+  /**
+  * AutoHide navigation bar
+  */
   static fulscreenMode() {
-      let autoHideNavigationBar = true;
-      window.navigationbar.setUp( autoHideNavigationBar );
+    let autoHideNavigationBar = true;
+    window.navigationbar.setUp( autoHideNavigationBar );
+  }
+  
+  /**
+  * Show toast with some message
+  * @param {string} message - user message for toast
+  */
+  static showToast = ( message ) => {
+    window.plugins.toast.showWithOptions({
+      message: message,
+      duration: "3000", 
+      position: "center",
+      styling: {
+        opacity: 0.75, 
+        textColor: '#FFFF00', 
+        textSize: 26.5, 
+        cornerRadius: 16, 
+      }
+    });
+  }
+  /**
+   * Set device mode (sleeped or active)
+   * @param {boolean} sleeped 
+   */
+  static setDeviceSleeping(sleeped){
+    const WifiManager = window.cordova.plugins.WifiManager
+    WifiManager.setWifiEnabled(!sleeped);
+   
+    WifiManager.onwifistatechanged = function (data) {
+      Device.showToast(`Wifi ${data.wifiState.toLocaleLowerCase()}!`);
+    }
   }
 }
 
