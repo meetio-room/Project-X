36c305276f270a2f675357ead4168acdf07ea3fa
DmytroRoik <dmytroroik@gmail.com>
2018-05-01 02:08:25 +0300

    add default avatar image

--- project-x/src/components/AttendeeItem/AttendeeItem.jsx
+++ project-x/src/components/AttendeeItem/AttendeeItem.jsx
@@ -6,7 +6,7 @@ import './AttendeeItem.css';
  */
 const attendeeItem = props => (
   <div className="AttendeeItem">
-    <img src={props.img} alt="" />
+    <img src={props.img || 'https://res.cloudinary.com/demo/image/upload/w_100,h_100,c_thumb,g_face,r_20,d_avatar.png/non_existing_id.png'} alt="" />
     <div className="AttendeeItem-name">{props.name}</div>
   </div>
 );
